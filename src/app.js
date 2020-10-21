5f7876c2c8204730fec86bfe6679513768ac83f9
GraceeFlower <1491644103@qq.com>
2020-10-21 13:10:12 +0800

    [Min] fix: adjust the api url

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import { getProfile } from "./script/askForData";
 
 const profileAvatar = document.getElementsByClassName("profile-avatar")[0];
 const profileDetail = document.getElementsByClassName("profile-detail")[0];
-const userId = document.URL.split("/").pop();
+const apiURL = document.URL.split("/").slice(3).join("/");
 
 const initProfile = (user) => {
   profileAvatar.src = user.avatar;
   profileDetail.innerText = `MY NAME IS ${user.name} ${user.age}YO AND THIS IS MY RESUME/CV`;
 };
 
-getProfile(userId).then((user) => initProfile(user));
+getProfile(apiURL).then((user) => initProfile(user));
--- src/script/askForData.js
+++ src/script/askForData.js
@@ -1,7 +1,7 @@
 const BASIC_URL = "http://localhost:8080";
 
-const getProfile = (userId) => {
-  return fetch(`${BASIC_URL}/users/${userId}`)
+const getProfile = (apiURL) => {
+  return fetch(`${BASIC_URL}/${apiURL}`)
     .then((res) => res.json())
     .then((data) => data)
     .catch((err) => console.log(err));
