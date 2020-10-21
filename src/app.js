d619477cea8bf90ec303e4f861fd1ebc75ca458f
GraceeFlower <1491644103@qq.com>
2020-10-21 16:05:02 +0800

    [Min] feat: get educations from BE

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import "./style/index.scss";
-import { getProfile } from "./script/askForData";
+import { getProfile, getEducations } from "./script/askForData";
 import defaultAvatar from "./static/default-avatar.jpg";
 
 const profileAvatar = document.getElementsByClassName("profile-avatar")[0];
 const profileDetail = document.getElementsByClassName("profile-detail")[0];
-const apiUrl = document.URL.split("/").slice(3).join("/");
+const educationDetail = document.getElementsByClassName("edu-detail")[0];
 
 const initProfile = (user) => {
   profileAvatar.src = user.avatar ? user.avatar : defaultAvatar;
@@ -13,4 +13,16 @@ const initProfile = (user) => {
   }YO AND THIS IS MY RESUME/CV`;
 };
 
-getProfile(apiUrl).then((user) => initProfile(user));
+const initEducation = (educations) => {
+  educations.reduce((pre, cur, index) => {
+    educationDetail.innerHTML += `<li class="education" id=${index}>
+       <aside class="edu-year">${cur.year}</aside>
+       <h3 class="edu-title">${cur.title}</h3>
+       <p class="edu-description">${cur.description}</p>
+     </li>`;
+    return pre;
+  }, "");
+};
+
+getProfile().then((user) => initProfile(user));
+getEducations().then((educations) => initEducation(educations));
--- src/script/askForData.js
+++ src/script/askForData.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
 const BASIC_URL = "http://localhost:8080";
+const apiUrl = document.URL.split("/").slice(3).join("/");
 
-const getProfile = (apiUrl) => {
+const getProfile = () => {
   return axios
     .get(`${BASIC_URL}/${apiUrl}`)
     .then((res) => res.data)
     .catch(() => "");
 };
 
-export { BASIC_URL, getProfile };
+const getEducations = () => {
+  return axios
+    .get(`${BASIC_URL}/${apiUrl}/educations`)
+    .then((res) => res.data)
+    .catch(() => "");
+};
+
+export { BASIC_URL, getProfile, getEducations };
