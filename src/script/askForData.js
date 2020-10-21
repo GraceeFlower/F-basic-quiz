8cae00d7dc96ecc352d61f3d0ab8d9ea51ec3bf1
GraceeFlower <1491644103@qq.com>
2020-10-21 14:33:01 +0800

    [Min] feat: use axios to get user profile

--- src/script/askForData.js
+++ src/script/askForData.js
@@ -1,10 +1,12 @@
+import axios from "axios";
+
 const BASIC_URL = "http://localhost:8080";
 
 const getProfile = (apiURL) => {
-  return fetch(`${BASIC_URL}/${apiURL}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((err) => console.log(err));
+  return axios
+    .get(`${BASIC_URL}/${apiURL}`)
+    .then((res) => res.data)
+    .catch((err) => err);
 };
 
 export { BASIC_URL, getProfile };
